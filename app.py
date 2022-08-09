68f7257dcf2355622730bad95a4fac8f8cd1607a
dhruvarora561 <dhruvarora561@gmail.com>
2022-08-10 01:43:15 +0530

    can now store and return links

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from concurrent.futures import thread
 from threading import Thread
+from wsgiref.util import request_uri
 from flask import Flask, render_template, request
 import platform, socket,re,uuid,json,psutil,logging
 from cpu_usage import *
@@ -28,7 +29,15 @@ def custom_scripts():
 
 @app.route('/hosted')
 def selfhosted():
-    return render_template('selfhosted.html')
+    
+    mydb=mysql.connector.connect( host='localhost', user=os.environ.get("user"),password=os.environ.get("password"), database='bifrost')
+    mycursor=mydb.cursor()
+    sql='select link from config where id=1'
+    mycursor.execute(sql)
+    result=mycursor.fetchall()
+    
+        
+    return render_template('selfhosted.html',result=result)
 
 @app.route('/settings', methods=['POST','GET'])
 def settings():
@@ -42,10 +51,7 @@ def settings():
         val=(link, name)
         mycursor.execute(sql, val)
         mydb.commit()
-        
-       
-
-        
+    #maybe put this in try and catch block and add a response drom the db if the value is added or not.     
     return render_template('settings.html')
 
 
